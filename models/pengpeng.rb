0ffc2d5902a7705b486baaf6ee9a3143ac563c53
teng.cong <congteng45@gmail.com>
2014-10-10 22:01:04 +0800

    en

--- models/pengpeng.rb
+++ models/pengpeng.rb
@@ -2,9 +2,11 @@ class Pengpeng
 
   PENG_TYPE = [
     "ad@@admob",
-    "add@@20"
+    "add@@20",
     # "video@@http://www.xxx.com/aaa.mp4",
     # "web@@http://baidu.com"
+    "video@@http://lol-video.b0.upaiyun.com/mm.mp4",
+    "video@@http://lol-video.b0.upaiyun.com/test.mp4"
   ]
 
   class << self
