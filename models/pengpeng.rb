65bfc9195a2dd7eeaaa09dec56c04ee9caec807b
teng.cong <congteng45@gmail.com>
2014-10-11 15:09:56 +0800

    return retina pics

--- models/pengpeng.rb
+++ models/pengpeng.rb
@@ -3,8 +3,7 @@ class Pengpeng
   PENG_TYPE = [
     "ad@@admob",
     "add@@20",
-    # "video@@http://www.xxx.com/aaa.mp4",
-    # "web@@http://baidu.com"
+    "web@@http://baidu.com",
     "video@@http://lol-video.b0.upaiyun.com/mm.mp4",
     "video@@http://lol-video.b0.upaiyun.com/test.mp4"
   ]
