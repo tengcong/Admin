5a22aa9dd2e2e61fc913970a541c831ed108374a
teng.cong <congteng45@gmail.com>
2015-12-18 17:36:55 +0800

    add password

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,10 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
+  if not Rails.env.development?
+    http_basic_authenticate_with name: "tengcong", password: "123456"
+  end
+
   def model
     self.class.to_s.gsub('Controller', '').singularize.constantize
   end
