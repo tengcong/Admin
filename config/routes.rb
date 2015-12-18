edca2c4930c3fcc977f153c693d897c673c0ce35
teng.cong <congteng45@gmail.com>
2015-12-18 21:52:15 +0800

    routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,4 @@
 Rails.application.routes.draw do
-
-
-  root to: "photos#index"
-
-
   # concern :listable do
   #   post :publish
   #   post :unpublish
@@ -16,26 +11,56 @@ Rails.application.routes.draw do
   #     delete :batch_destroy
   #   end
   # end
-  #
-  # resources :photos
-  # resources :albums, concerns: :listable do
-  #   resources :photos
-  #
-  #   collection do
-  #     get 'tags'
-  #     get 'hot'
-  #     get 'tagged_with/:tag' => 'albums#tagged_with'
-  #   end
-  # end
-  #
-  # resources :bqs
-  # resources :bq_packages, concerns: :listable do
-  #   resources :bqs
-  # end
-  #
-  # resources :bq_types do
-  #   resources :bq_packages
-  # end
-  #
-  # root to: "home#index"
+
+  resources :photos
+  resources :albums do
+    resources :photos
+
+    collection do
+      get 'tags'
+      get 'hot'
+      get 'tagged_with/:tag' => 'albums#tagged_with'
+    end
+
+
+
+
+    post :publish
+    post :unpublish
+    post :move_to
+
+    collection do
+      get :search
+      get :published
+      patch :batch_move
+      delete :batch_destroy
+    end
+
+
+  end
+
+  resources :bqs
+  resources :bq_packages do
+    resources :bqs
+
+
+    post :publish
+    post :unpublish
+    post :move_to
+
+    collection do
+      get :search
+      get :published
+      patch :batch_move
+      delete :batch_destroy
+    end
+
+
+  end
+
+  resources :bq_types do
+    resources :bq_packages
+  end
+
+  root to: "home#index"
 end
