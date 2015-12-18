cdcb9386d291154447d077089b06451228c8281e
teng.cong <congteng45@gmail.com>
2015-12-18 21:50:26 +0800

    test routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,36 +1,41 @@
 Rails.application.routes.draw do
-  concern :listable do
-    post :publish
-    post :unpublish
-    post :move_to
 
-    collection do
-      get :search
-      get :published
-      patch :batch_move
-      delete :batch_destroy
-    end
-  end
 
-  resources :photos
-  resources :albums, concerns: :listable do
-    resources :photos
+  root to: "photos#index"
 
-    collection do
-      get 'tags'
-      get 'hot'
-      get 'tagged_with/:tag' => 'albums#tagged_with'
-    end
-  end
 
-  resources :bqs
-  resources :bq_packages, concerns: :listable do
-    resources :bqs
-  end
-
-  resources :bq_types do
-    resources :bq_packages
-  end
-
-  root to: "home#index"
+  # concern :listable do
+  #   post :publish
+  #   post :unpublish
+  #   post :move_to
+  #
+  #   collection do
+  #     get :search
+  #     get :published
+  #     patch :batch_move
+  #     delete :batch_destroy
+  #   end
+  # end
+  #
+  # resources :photos
+  # resources :albums, concerns: :listable do
+  #   resources :photos
+  #
+  #   collection do
+  #     get 'tags'
+  #     get 'hot'
+  #     get 'tagged_with/:tag' => 'albums#tagged_with'
+  #   end
+  # end
+  #
+  # resources :bqs
+  # resources :bq_packages, concerns: :listable do
+  #   resources :bqs
+  # end
+  #
+  # resources :bq_types do
+  #   resources :bq_packages
+  # end
+  #
+  # root to: "home#index"
 end
