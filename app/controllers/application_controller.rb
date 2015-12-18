2c803bd0987bb6e09e4d8130045c2d0a704bbd97
teng.cong <congteng45@gmail.com>
2015-12-18 17:53:34 +0800

    api control

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,12 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
-  if not Rails.env.development?
-    http_basic_authenticate_with name: "tengcong", password: "123456"
+  before_filter do
+    unless Rails.env.development? || request.format == 'application/json'
+      authenticate_or_request_with_http_basic("ADMIN PANEL") do |name, password|
+        name == 'tengcong' && password == '123456'
+      end
+    end
   end
 
   def model
