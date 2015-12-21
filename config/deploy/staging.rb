8c17ed95ceeb29145e422ddceb96e0e352c7ce08
teng.cong <congteng45@gmail.com>
2015-12-21 14:02:44 +0800

    create index

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -17,4 +17,12 @@ set :enable_ssl, false
 
 set :whenever_identifier, ->{ "#{fetch(:application)}_#{fetch(:stage)}" }
 
+namespace :mongoid do
+  desc "Create MongoDB indexes"
+  task :index do
+    run "cd #{deploy_to} && bundle exec rake db:mongoid:create_indexes", :once => true
+  end
+end
+
+after "deploy:update", "mongoid:index"
 after 'deploy:publishing', 'puma:restart'
