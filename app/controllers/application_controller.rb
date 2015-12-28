ea59fb1f10161d081e11996325212a58e787d0fd
teng.cong <congteng45@gmail.com>
2015-12-28 17:45:42 +0800

    test

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,7 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
-  before_filter :auth do
+  before_filter :auth, except: ['home#app'] do
     unless Rails.env.development? || request.format == 'application/json'
       authenticate_or_request_with_http_basic("ADMIN PANEL") do |name, password|
         name == 'tengcong' && password == '123456'
--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,11 +1,6 @@
 class HomeController < ApplicationController
 
-  before_action :auth, except: ['app']
-
   def index
   end
 
-  def app
-    render text: 'home'
-  end
 end
